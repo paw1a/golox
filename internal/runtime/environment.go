c2a8764b2f9f4eab2ba018f27f9be825809aab69
paw1a <paw1a@yandex.ru>
2022-08-11 10:50:10 +0300

    resolver removed

--- internal/runtime/environment.go
+++ internal/runtime/environment.go
@@ -28,18 +28,6 @@ func (e Environment) get(name lexing.Token) interface{} {
 	return nil
 }
 
-func (e *Environment) getAt(distance int, name string) interface{} {
-	return e.ancestor(distance).objects[name]
-}
-
-func (e *Environment) ancestor(distance int) *Environment {
-	env := e
-	for i := 0; i < distance; i++ {
-		env = env.enclosing
-	}
-	return env
-}
-
 func (e Environment) assign(name lexing.Token, value interface{}) {
 	if _, ok := e.objects[name.Lexeme]; ok {
 		e.objects[name.Lexeme] = value
@@ -52,10 +40,6 @@ func (e Environment) assign(name lexing.Token, value interface{}) {
 	}
 
 	runtimeError(name, fmt.Sprintf("undefined variable '%s'", name.Lexeme))
-}
-
-func (e *Environment) assignAt(distance int, name lexing.Token, value interface{}) {
-	e.ancestor(distance).objects[name.Lexeme] = value
 }
 
 func NewEnvironment(enclosing *Environment) *Environment {
